3df52b2e48d036650d5e4fa47fb10f5db6df7a7d
baraziz <baharudinazizcode@gmail.com>
2023-10-05 01:13:39 +0700

    first commit

---
+++ admin/about.php
@@ -0,0 +1,13 @@
+<div class="container mt-4 border p-4 rounded-3 bg-primary-subtle">
+
+   <div class="text-center mb-3">
+      <img class="col-3 img-thumbnail" style="height: 275px;" src="https://icon-library.com/images/no-profile-picture-icon/no-profile-picture-icon-12.jpg" alt="">
+   </div>
+
+   <div class="col rounded-2 bg-white p-1">
+      <p style="text-align: justify; text-justify: inter-word;">
+         Website ini dibuat untuk memenuhi penugasan MSIB
+      </p>
+   </div>
+
+</div>
---
+++ admin/contact.php
@@ -0,0 +1,51 @@
+<div class="container my-4">
+   <form action="" class="border rounded-3 p-4 bg-body-tertiary">
+      <div class="row">
+
+         <div class="col bg-primary-subtle border rounded-start">
+            <h3 class="mb-4 mt-1">Contact Information</h3>
+
+            <div class="form-floating mb-3">
+               <input type="email" class="form-control" placeholder="name@example.com" value="baharudinazizcode@gmail.com" disabled>
+               <label for="floatingInputDisabled">Email address</label>
+            </div>
+
+            <div class="form-floating mb-3">
+               <input type="text" class="form-control" placeholder="name@example.com" value="+62 8xx 36xx xxxx" disabled>
+               <label for="floatingInputDisabled">No. Telp</label>
+            </div>
+
+         </div>
+
+         <div class="col-8 border-2 border-start">
+            <h3 class="mb-4 mt-1">Contact Us</h3>
+
+            <div class="row mb-3 mx-auto border rounded-3 p-2">
+               <label for="" class="mb-1">Name</label>
+               <div class="col">
+                  <label for="exampleFormControlInput1" class="form-label">First</label>
+                  <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="First Name">
+               </div>
+               <div class="col">
+                  <label for="exampleFormControlInput1" class="form-label">Last</label>
+                  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Last Name">
+               </div>
+            </div>
+
+            <div class="mb-3">
+               <label for="exampleFormControlInput1" class="form-label">Email address</label>
+               <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+            </div>
+            <div class="mb-3">
+               <label for="exampleFormControlTextarea1" class="form-label">Message</label>
+               <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+
+            <div class="text-end mt-5">
+               <input class="btn btn-primary btn-lg" type="submit" value="Submit">
+            </div>
+         </div>
+      </div>
+
+   </form>
+</div>
